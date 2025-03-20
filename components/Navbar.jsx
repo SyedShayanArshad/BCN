b17a9ca34a07d19366322ace15bd370bd630f712
SyedShayanArshad <syedshayanarshad.1@gmail.com>
2025-03-20 20:27:51 +0500

    update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,19 +17,19 @@ function Navbar() {
   return (
     <nav className="w-full px-4 py-3 border-b shadow-sm dark:shadow-gray-800">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
-        <h1 className="text-xl font-bold">BUKHARI CABLE NETWORK</h1>
+        <h1 className="text-lg md:text-xl font-bold">BUKHARI CABLE NETWORK</h1>
 
         <div className="flex items-center gap-4 sm:gap-6">
-          <button
+          {/* <button
             aria-label="Refresh"
             className="p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-full transition-colors"
           >
             <FaSyncAlt className="h-5 w-5 md:h-6 md:w-6" />
-          </button>
+          </button> */}
 
           <div className="flex items-center gap-2 hover:bg-gray-100 dark:hover:bg-gray-800 px-3 py-1 rounded-full transition-colors">
             <FaCircleUser className="h-6 w-6 md:h-7 md:w-7" />
-            <span className="hidden md:block text-sm font-medium">
+            <span className="block text-sm font-medium">
               {status === "loading"
                 ? "Loading..."
                 : session?.user?.name || "Guest"}
